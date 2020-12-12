f7c26c0620028c75ade2eb6461c664289b306644
Jack Allard <jack.allard@gmail.com>
2020-12-11 18:04:59 -0800

    Added functionality of displaying form fields on document

--- igxFormFieldsToolbar/Controllers/documentControls.cs
+++ igxFormFieldsToolbar/Controllers/documentControls.cs
@@ -1,6 +1,6 @@
 ﻿using Word = Microsoft.Office.Interop.Word;
 using Office = Microsoft.Office.Core;
-using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Interop.Word;
 using igxFormFieldsToolbar.Models;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
@@ -11,11 +11,11 @@ namespace igxFormFieldsToolbar
 {
     public partial class Controllers
     {
-        public static Document document = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
-        public static void GenerateInputFields(List<IGXSchema> schemas, int selection) //IGXSchema schema
+        public static Microsoft.Office.Tools.Word.Document document = Globals.Factory.GetVstoObject(Globals.ThisAddIn.Application.ActiveDocument);
+        public static void GenerateInputFields(List<IGXSchema> schemas, int selection)
         {
             if (selection != -1){
-                //generate all field objects for the selected schema
+                //parses list of fields from JSON and adds each one
                 IGXSchema selectedSchema = schemas[selection];
                 string selectedJSON = schemas[selection].jsonString;
                 var jObj = JObject.Parse(selectedJSON);
@@ -24,7 +24,7 @@ namespace igxFormFieldsToolbar
 
                 foreach (SchemaField field in fields)
                 {
-                    Debug.WriteLine("this is a field");
+                    addField(field.TypeName, field.Label, field.Required);
                 }
             } else
             {
@@ -33,23 +33,53 @@ namespace igxFormFieldsToolbar
 
         }
 
-        public static void addField(string label)
+        public static void addField(string typeName, string label, bool required)
         {
-            
-            
-        }
-            
+            //moves range each time field is added, generates text label over each field
+            object i = 1;
+            document.Paragraphs.First.Range.Select();
+            var range = document.Range();
+
+            range.Move(Unit: Word.WdUnits.wdParagraph, Count: ref i);
+            document.Paragraphs.Add();
+            range.Move(Unit: Word.WdUnits.wdParagraph, Count: ref i);
+
+            range.Text = label;
+
+            range.Move(Unit: Word.WdUnits.wdParagraph, Count: ref i);
+            document.Paragraphs.Add();
+            range.Move(Unit: Word.WdUnits.wdParagraph, Count: ref i);
+
+            ContentControl control;
+
+            //mapping CMS fields to word ContentControl fields
+            //using the typename string is def not the best way to do this....should probably create some kind of list/dictionary for all "_FieldTypeValue" in the JSON
+            if (typeName == "Text Element")
+            {
+                control = document.ContentControls.Add(WdContentControlType.wdContentControlText, range);
+                control.Color = WdColor.wdColorGray40;
+                control.LockContentControl = true;
+            } else if (typeName == "XHTML Element")
+            {
+                control = document.ContentControls.Add(WdContentControlType.wdContentControlRichText, range);
+                control.Color = WdColor.wdColorGray40;
+                control.LockContentControl = true;
+            } else if (typeName == "Checkbox")
+            {
+                control = document.ContentControls.Add(WdContentControlType.wdContentControlCheckBox, range);
+                control.Color = WdColor.wdColorGray40;
+                control.LockContentControl = true;
+            } else if (typeName == "Asset" || typeName.Contains("Image") == true)
+            {
+                control = document.ContentControls.Add(WdContentControlType.wdContentControlPicture, range);
+                control.Color = WdColor.wdColorGray40;
+                control.LockContentControl = true;
+            }
 
         }
+
+
+    }
 
         
 }
-
-//string fieldLabel = "field";
-//int fieldNumber = 1;
-//document.Controls.AddPlainTextContentControl($"{fieldLabel}{ fieldNumber}");
-//fieldNumber++;
-
-//var jObj = JObject.Parse(selectedJSON);
-//var fields = jObj["_CurrentVersion"]["_Fields"]
-//    .ToObject<List<SchemaField>>();
